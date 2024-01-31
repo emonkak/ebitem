34eab56f894a994ef7509b1cb177d7091e97c421
Shota Nozaki <emonkak@gmail.com>
2024-02-01 02:23:06 +0900

    feat(directives.classList): remove class names that do not exist

--- src/directives/classList.ts
+++ src/directives/classList.ts
@@ -1,15 +1,24 @@
 import { Directive, directiveSymbol } from '../directive';
-import { Part } from '../part';
+import type { Part } from '../part';
 import { AttributePart } from '../parts';
 import type { Effect, Updater } from '../updater';
 
-export type ClassSpecifier = string | { [key: string]: boolean };
+export type ClassMap = { [key: string]: boolean };
+
+export type ClassSpecifier = string | ClassMap;
 
 export class ClassList implements Directive {
-  private readonly _classSpecifiers: ClassSpecifier[];
+  private readonly _classMap: ClassMap;
 
   constructor(classSpecifiers: ClassSpecifier[]) {
-    this._classSpecifiers = classSpecifiers;
+    this._classMap = classSpecifiers.reduce<ClassMap>((acc, classSpecifier) => {
+      if (typeof classSpecifier === 'string') {
+        acc[classSpecifier] = true;
+      } else {
+        Object.assign(acc, classSpecifier);
+      }
+      return acc;
+    }, {});
   }
 
   [directiveSymbol](part: Part, updater: Updater): void {
@@ -19,37 +28,43 @@ export class ClassList implements Directive {
       );
     }
 
-    updater.pushMutationEffect(
-      new UpdateClassList(part, this._classSpecifiers),
-    );
+    updater.pushMutationEffect(new UpdateClassList(part, this._classMap));
   }
 }
 
 class UpdateClassList implements Effect {
   private _part: AttributePart;
 
-  private _classSpecifiers: ClassSpecifier[];
+  private _classMap: ClassMap;
 
-  constructor(part: AttributePart, classSpecifiers: ClassSpecifier[]) {
+  constructor(part: AttributePart, classMap: ClassMap) {
     this._part = part;
-    this._classSpecifiers = classSpecifiers;
+    this._classMap = classMap;
   }
 
   commit(_updater: Updater): void {
     const { classList } = this._part.node;
 
-    for (let i = 0, l = this._classSpecifiers.length; i < l; i++) {
-      const classSpecifier = this._classSpecifiers[i];
-      if (typeof classSpecifier === 'string') {
-        classList.add(classSpecifier);
+    for (let i = 0, l = classList.length; i < l; i++) {
+      const className = classList[i]!;
+
+      if (
+        Object.hasOwn(this._classMap, className) &&
+        !this._classMap[className]
+      ) {
+        classList.remove(className);
+      }
+    }
+
+    const newClassNames = Object.keys(this._classMap);
+
+    for (let i = 0, l = newClassNames.length; i < l; i++) {
+      const newClassName = newClassNames[i]!;
+
+      if (this._classMap[newClassName]) {
+        classList.add(newClassName);
       } else {
-        for (const className in classSpecifier) {
-          if (classSpecifier[className]) {
-            classList.add(className);
-          } else {
-            classList.remove(className);
-          }
-        }
+        classList.remove(newClassName);
       }
     }
   }
